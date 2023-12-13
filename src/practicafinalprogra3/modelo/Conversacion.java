9dba8df5711ea70527dafeb79a520fa84e9a69fa
MarioCaballeroBenavente <MarioCaballeroBenavente@usal.es>
2023-12-13 13:34:53 +0100

    (Implementados constructor, getters y setters de Conversacion)

--- src/practicafinalprogra3/modelo/Conversacion.java
+++ src/practicafinalprogra3/modelo/Conversacion.java
@@ -18,4 +18,42 @@ public class Conversacion implements Serializable {
     String tipoLLM;
     long fechaIni;
     long fechaFin;
+
+    public Conversacion(long fechaIni) {
+        this.fechaIni = fechaIni;
+    }
+
+    public List<Mensaje> getMensajes() {
+        return mensajes;
+    }
+
+    public void setMensajes(List<Mensaje> mensajes) {
+        this.mensajes = mensajes;
+    }
+
+    public String getTipoLLM() {
+        return tipoLLM;
+    }
+
+    public void setTipoLLM(String tipoLLM) {
+        this.tipoLLM = tipoLLM;
+    }
+
+    public long getFechaIni() {
+        return fechaIni;
+    }
+
+    public void setFechaIni(long fechaIni) {
+        this.fechaIni = fechaIni;
+    }
+
+    public long getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(long fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+    
+    
 }
